cae033d5fdac734980c1dbe1dd4a9f413bacad3b
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-10-31 13:22:23 +0800

    deployment fix

--- src/Components/CallApi.jsx
+++ src/Components/CallApi.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import PokeCard from "./Pokecard.jsx";
+import PokeCard from "./PokeCard.jsx";
 
 export default function CallApi() {
   const [pokemon, setPokemon] = useState([]);
