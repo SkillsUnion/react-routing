275c66d42e8aae2c7ba22dd937c3e7213e547a0a
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-10-31 13:48:03 +0800

    deployment routess IV

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/api">Api</Link>
-      <Link to="/profile">Profile</Link>
-      <Link to="/component">Component</Link>
+      <Link to="/react-routing-3.2/">Home</Link>
+      <Link to="/react-routing-3.2/api">Api</Link>
+      <Link to="/react-routing-3.2/profile">Profile</Link>
+      <Link to="/react-routing-3.2/component">Component</Link>
     </div>
   );
 }
