d2b2ac8864d1fed433b08e5e9a8f6cd96e9f7ccc
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-10-31 11:47:58 +0800

    cleanning

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import Navbar from "./Components/Navbar.jsx";
 import CallApi from "./Components/CallApi.jsx";
 import Root from "./Components/Root.jsx";
 import Profile from "./Components/Profile.jsx";
@@ -11,103 +10,17 @@ import "./App.css";
 import {
   createBrowserRouter,
   RouterProvider,
-  // Navigate,
+  Navigate,
   createRoutesFromElements,
   Route,
 } from "react-router-dom";
 
 export default function App() {
-  // const router = createBrowserRouter([
-  //   {
-  //     path: "/",
-  //     element: (
-  //       <div>
-  //         <Navbar />
-  //         <h1>Hello World</h1>
-  //       </div>
-  //     ),
-  //   },
-  //   {
-  //     path: "api",
-  //     element: (
-  //       <div>
-  //         <Navbar />
-  //         <h1>Hello Api</h1>
-  //       </div>
-  //     ),
-  //   },
-  //   {
-  //     path: "profile",
-  //     element: (
-  //       <div>
-  //         <Navbar />
-  //         <h1>Profile</h1>
-  //       </div>
-  //     ),
-  //   },
-
-  //   {
-  //     path: "component",
-  //     element: (
-  //       <div>
-  //         <Navbar />
-  //         <h1>Component</h1>
-  //       </div>
-  //     ),
-  //   },
-  // ]);
-
-  // Nested routes in Root
   const RequireAuth = ({ children, redirectTo, user }) => {
     console.log(user);
     const isAuthenticated = user ? true : false;
     return isAuthenticated ? children : <Navigate to={redirectTo} />;
   };
-
-  // const router = createBrowserRouter([
-  //   {
-  //     path: "/",
-  //     element: <Root />,
-  //     errorElement: <ErrorPage />,
-  //     children: [
-  //       { path: "/", element: <Home /> },
-
-  //       { path: "api", element: <CallApi /> },
-  //       {
-  //         path: "profile",
-  //         element: (
-  //           // <RequireAuth redirectTo={"/"} user={true}>
-  //           <Profile />
-  //           // </RequireAuth>
-  //         ),
-  //         children: [
-  //           {
-  //             path: "edit",
-  //             element: (
-  //               <div>
-  //                 <h3>Edit Profile</h3>
-  //                 <p>Edit me now</p>
-  //               </div>
-  //             ),
-  //           },
-
-  //           {
-  //             path: "view",
-  //             element: (
-  //               <div>
-  //                 <h3>View Profile</h3>
-  //                 <p>View me now</p>
-  //               </div>
-  //             ),
-  //           },
-  //         ],
-  //       },
-  //       { path: "component", element: <Component /> },
-  //     ],
-  //   },
-  // ]);
-
-  // nest routes with createRoutesFromElements
 
   const router = createBrowserRouter(
     createRoutesFromElements(
@@ -117,9 +30,9 @@ export default function App() {
         <Route
           path="/profile"
           element={
-            // <RequireAuth redirectTo={"/"} user={false}>
-            <Profile />
-            // </RequireAuth>
+            <RequireAuth redirectTo={"/"} user={false}>
+              <Profile />
+            </RequireAuth>
           }
         >
           <Route
